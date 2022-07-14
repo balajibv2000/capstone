eec86ed61d5b5fb690857dad59808166c39f0b71
CBT <balaji.bv2000@gmail.com>
2022-07-14 12:43:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var http = require('http'); // 1 - Import Node.js core module
+
+var server = http.createServer(function (req, res) {   // 2 - creating server
+
+    //handle incomming requests here..
+    console.log("got req");
+    res.write("hello");
+    
+    res.end();
+
+});
+
+server.listen(9000); //3 - listen for any incoming requests
+
+console.log('Node.js web server at port 5000 is running..')
