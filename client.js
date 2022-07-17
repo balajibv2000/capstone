2e98034d8894a2188e72cc8dfcd75ef22f168273
balajibv2000 <57826017+balajibv2000@users.noreply.github.com>
2022-07-17 13:57:02 +0530

    Delete client.js

--- client.js
+++
@@ -1,8 +0,0 @@
-const coap = require('coap') // or coap
-const req = coap.request('coap://[fd00::202:2:2:2]/test/hello')
-
-req.on('response', (res) => {
-    res.pipe(process.stdout)
-})
-
-req.end()
