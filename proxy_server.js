1b71fc92c7758dc5f166109890de4e25fecc800b
balajibv2000 <57826017+balajibv2000@users.noreply.github.com>
2022-07-23 12:20:23 +0530

    Add files via upload

--- proxy_server.js
+++ proxy_server.js
@@ -5,34 +5,84 @@ let cors = require("cors")
 const app = express()
 app.use(cors())
 
-const port = 5002
-
-const pid = [1,2,3,4,5]
-
+const port = 5000
 
 app.get('/critical', async (request, response) => {
 
-	const critical_data = []
-	count = 0;
+	const critical_data = [] 
+	var payload = ''
 
-	for (x in pid){
-		const id = pid[x]
-		const payload = {pid: id , data: ''}
+	const node = coap.request('coap://[fd00::201:1:1:1]/node/critical')
 
-		const node = coap.request(`coap://[fd00::20${id}:${id}:${id}:${id}]/node/critical`)
-		node.on('response', (res) => {
-			res.on('data' , (chunck) => {
-					payload.data += chunck.toString()			
-				})
-				
-			res.on('end' , () => {
-				count++
-				critical_data.push(payload)
-				if(count === pid.length) response.send(critical_data)
+	node.on('response', (res) => {
+		res.on('data' , (chunck) => {
+				payload += chunck.toString()			
 			})
+			
+		res.on('end' , () => {
+			const arr = payload.split(';')
+			arr.pop()
+			for( x in arr){
+				const data = arr[x].split(' ')
+				const obj = {pid: parseInt(data[0]) , oxy: parseInt(data[1]) , temp: parseInt(data[2]) , pulse: parseInt(data[3]) , bp: parseInt(data[4])}
+				critical_data.push(obj)
+			}
+			response.send(critical_data)
 		})
-		node.end()
-	}
+	})
+	node.end()
+
+})
+
+app.get('/mid-critical', async (request, response) => {
+
+	const mid_critical_data = []
+	var payload = ''
+
+	const node = coap.request('coap://[fd00::201:1:1:1]/node/mid-critical')
+
+	node.on('response', (res) => {
+		res.on('data' , (chunck) => {
+				payload += chunck.toString()			
+			})
+			
+		res.on('end' , () => {
+			const arr = payload.split(';')
+			for( x in arr){
+				const data = arr[x].split(' ')
+				const obj = {pid: data[0] , data: data[1]}
+				if(obj.data != null) mid_critical_data.push(obj)
+			}
+			response.send(mid_critical_data)
+		})
+	})
+	node.end()
+
+})
+
+app.get('/periodic', async (request, response) => {
+
+	const periodic_data = []
+	var payload = ''
+
+	const node = coap.request('coap://[fd00::201:1:1:1]/node/periodic')
+
+	node.on('response', (res) => {
+		res.on('data' , (chunck) => {
+				payload += chunck.toString()			
+			})
+			
+		res.on('end' , () => {
+			const arr = payload.split(';')
+			for( x in arr){
+				const data = arr[x].split(' ')
+				const obj = {pid: data[0] , data: data[1]}
+				if(obj.data != null) periodic_data.push(obj)
+			}
+			response.send(periodic_data)
+		})
+	})
+	node.end()
 
 })
 
